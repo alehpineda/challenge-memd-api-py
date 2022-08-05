d1d399d71f304e5862f2d2616062c2c60ac02929
Alex Pineda <h0h04dt@homeoffice.wal-mart.com>
2022-08-05 00:57:30 -0500

    downgrade flake8-html to py3.9

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@ import sys
 import os
 
 # Fix for relative paths
-sys.path.insert(0, os.path.abspath(
-    os.path.join(
-        os.path.dirname(__file__), '../app')))
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../app"))
+)
--- tests/test_member_controller.py
+++ tests/test_member_controller.py
@@ -2,15 +2,10 @@ import pytest
 from fastapi.testclient import TestClient
 
 from app.main import app
-from app.controllers.member_controller import (
-    retrieve_member_controller,
-    create_primary_member_controller,
-    create_dependent_member_controller,
-)
-
 
 # Fastapi test client
 client = TestClient(app)
+
 
 # Fixtures
 @pytest.fixture()
